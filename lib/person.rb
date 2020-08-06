d59cce6bfb458f7204cd172a3b14da02bbdcd50b
Bella Evalle <bellaevalle@yahoo.com>
2020-08-06 01:17:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
 # your code goes here
+class Person
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def get_paid
+  end
+
+  def take_bath
+  end
+
+  def work_out
+  end
+
+  def call_friend
+  end 
+  
+  def start_conversation
+  end 
+
+end
